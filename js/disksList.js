eb9a2227f4edd7ea07ecf78a91ba3d23cf107b9f
cuipengfei <cuipengfei2008@gmail.com>
2014-11-15 17:34:30 +0800

    search

--- js/disksList.js
+++ js/disksList.js
@@ -1,59 +1,79 @@
-$(document).ready(function(){
-  dataToView();
+$(document).ready(function() {
+  $("#search").keyup(function() {
+    search();
+  });
+
+  dataToView(disks);
 });
 
-function dataToView(){
+function dataToView(disks) {
   $("#disks").empty();
 
-  _.chain(disks).map(function(disk){
+  _.chain(disks).map(function(disk) {
     return diskDiv(disk);
-  }).each(function(div){
+  }).each(function(div) {
     $("#disks").append(div);
   });
 }
 
 function diskDiv(disk) {
   return $("<div>")
-  .attr("class","disk1")
-  .append(diskTitle(disk.name))
-  .append(diskImg(disk.img))
-  .append(diskDesc(disk.desc))
-  .append(removeButton(disk.name));
+    .attr("class", "disk1")
+    .append(diskTitle(disk.name))
+    .append(diskImg(disk.img))
+    .append(diskDesc(disk.desc))
+    .append(removeButton(disk.name));
 }
 
-function diskTitle(name){
+function diskTitle(name) {
   return $("<h3>").html(name);
 }
 
-function diskImg(url){
-  return $("<img>").attr("src",url);
+function diskImg(url) {
+  return $("<img>").attr("src", url);
 }
 
-function diskDesc(desc){
+function diskDesc(desc) {
   return $("<p>").html(desc);
 }
 
-function removeButton(name){
+function removeButton(name) {
   return $("<button>").html("remove")
-  .click(function(){
-    remove(name);
-  });
+    .click(function() {
+      remove(name);
+    });
 }
 
-function remove(name){
-  disks = _.chain(disks).filter(function(disk){
-    return disk.name!==name;
+function remove(name) {
+  disks = _.chain(disks).filter(function(disk) {
+    return disk.name !== name;
   });
 
-  dataToView();
+  dataToView(disks);
 }
 
-function add(){
-  var newProduct =
-  {name:$("#productName").val(),
-  img:$("#productImgUrl").val(),
-  desc:$("#productDesc").val()};
+function add() {
+  var newProduct = {
+    name: $("#productName").val(),
+    img: $("#productImgUrl").val(),
+    desc: $("#productDesc").val()
+  };
+
   disks.push(newProduct);
 
-  dataToView();
+  dataToView(disks);
+}
+
+function search() {
+  var keyWord = $("#search").val();
+  if (keyWord === "") {
+    dataToView(disks);
+  } else {
+    var searchedDisks = _.chain(disks).filter(function(disk) {
+      return disk.name.indexOf(keyWord) !== -1 ||
+        disk.desc.indexOf(keyWord) !== -1;
+    });
+
+    dataToView(searchedDisks);
+  }
 }
