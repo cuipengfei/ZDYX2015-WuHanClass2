86fa97fcbb2d051202bcca730120c11b844e2f80
cuipengfei <cuipengfei2008@gmail.com>
2014-11-15 15:39:48 +0800

    remove

---
+++ js/disksList.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  dataToView();
+});
+
+function dataToView(){
+  $("#disks").empty();
+
+  _.chain(disks).map(function(disk){
+    return diskDiv(disk);
+  }).each(function(div){
+    $("#disks").append(div);
+  });
+}
+
+function diskDiv(disk) {
+  return $("<div>")
+  .attr("class","disk1")
+  .append(diskTitle(disk.name))
+  .append(diskImg(disk.img))
+  .append(diskDesc(disk.desc))
+  .append(removeButton(disk.name));
+}
+
+function diskTitle(name){
+  return $("<h3>").html(name);
+}
+
+function diskImg(url){
+  return $("<img>").attr("src",url);
+}
+
+function diskDesc(desc){
+  return $("<p>").html(desc);
+}
+
+function removeButton(name){
+  return $("<button>").html("remove")
+  .click(function(){
+    remove(name);
+  });
+}
+
+function remove(name){
+  disks = _.chain(disks).filter(function(disk){
+    return disk.name!==name;
+  });
+
+  dataToView();
+}
