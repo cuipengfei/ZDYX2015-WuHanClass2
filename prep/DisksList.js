a145d435c6a74cf37b9bb3def3ba56b1cc2f32df
cuipengfei <cuipengfei2008@gmail.com>
2014-11-17 09:39:30 +0800

    用干巴巴的数据生成新的页面元素

--- prep/DisksList.js
+++ prep/DisksList.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
   var container = $("#disksContainer");
   console.log(container);
+  console.log(diskDiv(disks[0]))
 });
+
+function diskDiv(disk) {
+  return $("<div>")
+    .attr("class", "disk1")
+    .append(diskTitle(disk.name))
+    .append(diskImg(disk.img))
+    .append(diskDesc(disk.desc));
+}
+
+function diskTitle(name) {
+  return $("<h3>").html(name);
+}
+
+function diskImg(url) {
+  return $("<img>").attr("src", url);
+}
+
+function diskDesc(desc) {
+  return $("<p>").html(desc);
+}
