f22d18bfdd880f8ce705c49e850ba7bd3e3351ee
cuipengfei <cuipengfei2008@gmail.com>
2014-11-22 14:01:12 +0800

    generate content

--- class/DiskList.js
+++ class/DiskList.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
   var container = $("#container");
-  console.log(container);
+
+  console.log(diskDiv(disks[0]));
 });
+
+function diskDiv(disk) {
+  return $("<div>")
+    .attr("class", "disk1")
+    .append(diskTitle(disk.name))
+    .append(diskImg(disk.img))
+    .append(diskDesc(disk.desc));
+}
+
+function diskTitle(title) {
+  return $("<h3>").html(title);
+}
+
+function diskImg(img) {
+  return $("<img>").attr("src", img);
+}
+
+function diskDesc(desc) {
+  return $("<p>").html(desc);
+}
